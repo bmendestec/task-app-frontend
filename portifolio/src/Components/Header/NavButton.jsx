bff809d43dd32a45601ae443db5449a22a918847
Bruno Mendes <bmendestecnologia@gmail.com>
2024-05-26 10:10:21 -0300

    Portfolio update

--- portifolio/src/Components/Header/NavButton.jsx
+++ portifolio/src/Components/Header/NavButton.jsx
@@ -20,7 +20,7 @@ function NavButton(onData, NavPosition) {
     <svg
       width="40"
       height="40"
-      viewBox="0 0 40 40"
+      viewBox="0 0 24 24"
       xmlns="http://www.w3.org/2000/svg"
       xmlnsXlink="http://www.w3.org/1999/xlink"
       className={
@@ -39,7 +39,7 @@ function NavButton(onData, NavPosition) {
     <svg
       width="40"
       height="40"
-      viewBox="0 0 40 40"
+      viewBox="0 0 24 24"
       xmlns="http://www.w3.org/2000/svg"
       xmlnsXlink="http://www.w3.org/1999/xlink"
       className={
