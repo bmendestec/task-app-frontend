261fb62c534dd7ab79e7970ee49c134b6825d340
Mendes <bruno.paulamendes@philips.com>
2025-04-28 00:39:22 -0300

    Update

--- src/App.jsx
+++ src/App.jsx
@@ -15,7 +15,7 @@ function App() {
       <Routes>
         <Route path="/" element={<ProtectedRoute><Initial /></ProtectedRoute>} />
         <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
+        <Route path="/register" element={<ProtectedRoute><Register /></ProtectedRoute>} />
         <Route path="/home" element={<ProtectedRoute><Home /></ProtectedRoute>} />
         <Route path="/settings" element={<ProtectedRoute><Settings /></ProtectedRoute>} />
         <Route path="/usuarios" element={<ProtectedRoute><Usuarios /></ProtectedRoute>} />
